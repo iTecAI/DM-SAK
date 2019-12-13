04d77d8b563089fde712635370d59a469bd1de49
iTecAI <matteovh@gmail.com>
2019-12-12 20:01:52 -0500

    add map editing, scrolling, more colors, etc

--- mapper/__init__.py
+++ mapper/__init__.py
@@ -1,4 +1,4 @@
-import pygame,os
+import pygame,os,time,json
 pygame.init()
 
 #init constants
@@ -19,17 +19,32 @@ class Surface(pygame.Surface):
 class Button:
     def __init__(self,rect,text,clickf,color=(222, 222, 222)):
         global FONTS
+        self.is_clicked = False
         self.surface = pygame.Surface(rect.size)
         self.surface.fill(color)
         txtr = FONTS['primary'].render(text,True,(0,0,0))
-        tx = int((rect.w - txtr.width)/2)
-        ty = int((rect.h - txtr.height)/2)
+        tx = int((rect.w - txtr.get_width())/2)
+        ty = int((rect.h - txtr.get_height())/2)
         self.surface.blit(txtr,(tx,ty))
         self.click = clickf
         self.rect = rect
     def check(self):
-        if self.rect.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0]:
+        if self.rect.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0] and not self.is_clicked:
             self.click()
+            self.is_clicked = True
+        if not (self.rect.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0]):
+            self.is_clicked = False
+
+class imgButton(Button):
+    def __init__(self,rect,path,clickf,color=(222, 222, 222)):
+        global FONTS
+        self.is_clicked = False
+        self.surface = pygame.Surface(rect.size)
+        self.surface.fill(color)
+        img = pygame.transform.scale(pygame.image.load(path),rect.size)
+        self.surface.blit(img,rect.topleft)
+        self.click = clickf
+        self.rect = rect
 
 def tile_click_factory(tile):
     def f():
@@ -46,12 +61,12 @@ def load_sprites_folder(path,sz=[16,16]):
             sheet = pygame.image.load(os.path.join(path,i))
             for x in range(int(sheet.get_width()/sz[0])):
                 for y in range(int(sheet.get_height()/sz[1])):
-                    s = Surface(sz)
+                    s = Surface(sz,pygame.SRCALPHA)
                     s.blit(sheet,[0,0],area=pygame.Rect(x*16,y*16,16,16))
                     has_color = False
                     for nx in range(s.get_width()):
                         for ny in range(s.get_height()):
-                            if s.get_at((nx,ny)) != (0,0,0):
+                            if s.get_at((nx,ny))[:3] != (0,0,0):
                                 has_color = True
                     if has_color:
                         setattr(s,'tilenum',c)
@@ -66,30 +81,87 @@ class tiles:
         self.tilepos = 0
         self.mx = mx
     def inc(self):
-        self.tilepos += 10
+        self.tilepos += 4
         if self.tilepos > self.mx:
-            self.tilepos = mx
+            self.tilepos = self.mx
     def dec(self):
-        self.tilepos -= 10
+        self.tilepos -= 4
         if self.tilepos < 0:
             self.tilepos = 0
 
+def constrain(val,mini,maxi):
+    if val < mini:
+        return mini
+    if val > maxi:
+        return maxi
+    return val
+
+class Tile:
+    def __init__(self,rotation,tile,pos):
+        self.tile = pygame.transform.rotate(tile,rotation)
+        self.id = tile.tilenum
+        self.rot = rotation
+        self.pos = pos
+    def jsonize(self):
+        return json.dumps({
+            'tile':self.id,
+            'rotation':self.rot,
+            'position':self.pos
+        })
+    def getdict(self):
+        return {
+            'tile':self.id,
+            'rotation':self.rot,
+            'position':self.pos,
+            'rendered':self.tile,
+            'object':self
+        }
+
+
 #main function
-def main(size=[1600,800]):
+def main(size=[1920,1080]):
     SPRITES = []
     SPRITES.extend(load_sprites_folder(os.path.join('assets','tiles','DawnLike','Objects')))
     SPRITES.extend(load_sprites_folder(os.path.join('assets','tiles','DawnLike','Items')))
-    tls = tiles(len(SPRITES))
+    splus = 116
+    tls = tiles(len(SPRITES)-splus)
     print('Loaded sprites:',len(SPRITES))
 
     #prepare GUI
-    screen = pygame.display.set_mode(size,pygame.RESIZABLE)
+    screen = pygame.display.set_mode(size,pygame.FULLSCREEN)
     scsurf = pygame.Surface(size)
+    up_button = Button(pygame.Rect(0,0,146,30),'UP',tls.dec)
+    down_button = Button(pygame.Rect(0,1050,146,30),'DOWN',tls.inc)
+    #delete_button = imgButton(pygame.Rect(170,1034,32,32))
+    tileMap = {}
+    current_rotation = 0
+    relative_tile_pos = [0,0]
+    relative_pos = [0,0]
+    
     running = True
     while running:
-        scsurf.fill([0,0,0])
+        scsurf.fill([255,255,255])
+        mp = pygame.mouse.get_pos()
+        scr_cur_tile_pos = [constrain(round((mp[0]-16)/32)*32,160,1920),constrain(round((mp[1]-16)/32)*32,0,992)]
+        relative_tile_pos = [int(scr_cur_tile_pos[0]/32) + relative_pos[0],int(scr_cur_tile_pos[1]/32) + relative_pos[1]]
+        if state_inst.current_tile:
+            scsurf.blit(pygame.transform.scale(state_inst.current_tile,[32,32]),scr_cur_tile_pos)
+
+        for p in list(tileMap.keys()):
+            for layer in tileMap[p]:
+                bp = [(layer['position'][0]-relative_pos[0])*32,(layer['position'][1]-relative_pos[1])*32]
+                scsurf.blit(pygame.transform.scale(layer['rendered'],[32,32]),bp)
+
+
+        #menus
+        scsurf.fill([232, 232, 232],rect=pygame.Rect(0,0,146,1080))
+        scsurf.fill([171, 171, 171],rect=pygame.Rect(146,0,14,1080))
+        scsurf.fill([171, 171, 171],rect=pygame.Rect(160,1024,1760,56))
+        scsurf.blit(up_button.surface,up_button.rect.topleft)
+        scsurf.blit(down_button.surface,down_button.rect.topleft)
+        up_button.check()
+        down_button.check()
         #load tilebar
-        splus = 352
         while True:
             try:
                 sprslice = SPRITES[tls.tilepos:tls.tilepos+splus]
@@ -98,18 +170,54 @@ def main(size=[1600,800]):
                 splus -= 1
         scount = 1
         x = 2
-        y = 2
+        y = 34
         for tile in sprslice:
-            scsurf.blit(tile,[x,y])
-            x += 18
-            if scount > 7:
+            scsurf.blit(pygame.transform.scale(tile,[32,32]),[x,y])
+            if pygame.Rect(x,y,32,32).collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0]:
+                tile.click_func()
+            x += 34
+            if scount > 3:
                 scount = 0
-                y += 18
+                y += 34
                 x = 2
             scount += 1
+
         screen.blit(scsurf,[0,0])
+
         pygame.display.flip()
-        pygame.event.pump()
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.pos[0] <= 146:
+                    if event.button == 4:
+                        tls.dec()
+                    if event.button == 5:
+                        tls.inc()
+                if event.button == 1 and event.pos[0]>160 and event.pos[1]<1024 and state_inst.current_tile:
+                    tile_temp = Tile(current_rotation,state_inst.current_tile,relative_tile_pos).getdict()
+                    if tuple(relative_tile_pos) in tileMap.keys():
+                        tileMap[tuple(relative_tile_pos)].append(tile_temp)
+                    else:
+                        tileMap[tuple(relative_tile_pos)] = [tile_temp]
+                if event.button == 3 and event.pos[0]>160 and event.pos[1]<1024:
+                    if tuple(relative_tile_pos) in tileMap.keys():
+                        if len(tileMap[tuple(relative_tile_pos)]) > 0:
+                            tileMap[tuple(relative_tile_pos)].pop()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    relative_pos[1] -= 1
+                if event.key == pygame.K_DOWN:
+                    relative_pos[1] += 1
+                if event.key == pygame.K_LEFT:
+                    relative_pos[0] -= 1
+                if event.key == pygame.K_RIGHT:
+                    relative_pos[0] += 1
+                
+                
+        
 
 
 
